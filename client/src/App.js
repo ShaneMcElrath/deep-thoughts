393e02c47cb6a945a5fa3e6f78260d8ec96a0410
Shane McElrath <shane@rangermac.com>
2022-03-14 15:14:40 -0400

    Finished thought list, user page, reaction list and friendlist. Started login and signup.

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,13 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -18,13 +23,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <BrowserRouter>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile/:username" element={<Profile />} />
+              <Route path="/profile/" element={<Profile />} />
+              <Route path="/thought/:id" element={<SingleThought />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
